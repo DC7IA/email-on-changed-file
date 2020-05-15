5493248ba108d7e2d39c9902c7fecd2ffb924406
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-05-16 01:58:27 +0200

    fixed some wrong variables

--- email-on-changed-file.sh
+++ email-on-changed-file.sh
@@ -6,15 +6,15 @@ else
 echo "please specify the parameter file like this: ./email-on-changed-file.sh email-on-changed-file.vars"
 exit 1
 fi
-BASENAME="${FILENAME%.*}"
+BASENAME="${1%.*}"
 cd $DATADIR
-curl -sS $URL -o $BASENAME/$FILENAME
-if [ "$(md5sum $BASENAME/$FILENAME)" == "$(cat $MD5SUM)" ]
+curl -sS $URL -o $1/$FILENAME
+if [ "$(md5sum $1/$FILENAME)" == "$(cat $MD5SUM)" ]
 then
 echo no change
 exit
 else
-md5sum afu-pruefungen-files/Termine_Afu_Pruefungen.pdf > $MD5SUM
+md5sum $1/$FILENAME > $MD5SUM
 echo changed!
 printf "$CONTENT" | mail -s "$SUBJECT" -S from="$SENDER" $RECEIVER
 fi
